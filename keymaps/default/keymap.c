57c7629e95f46a407f86cd1b48470dd2d5182cad
Thilo Barth <thilo.b@rth.pm>
2024-08-30 08:45:37 +0200

    change layout

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,13 +3,13 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _QWERTY,
+    _LAYER1,
     _LOWER,
-    _RAISE,
     _ADJUST
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define LAYER1 MO(_LAYER_1)
+#define LOWER MO(_RAISE)
 #define ADJUST MO(_ADJUST)
 
 // Defines for task manager and such
@@ -30,35 +30,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  Z   |  X   |  C   |  V   |  B   |           |  N   |  M   |  ,   |  .   |  /   |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.       ,--------------------.
- *                  | ESCAPE | SHIFT| Space|    | LOWER | RAISE| ADJUST |
+ *                  | SHIFT | SPACE | ENTER|    | LOWER | RAISE| ADJUST |
  *                  `--------------------'      `--------------------.
  */
 [_QWERTY] = LAYOUT_split_3x5_3(
   KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,
   KC_A,    KC_S,    KC_D,    KC_F,      KC_G,           KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN,
   KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,           KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                    KC_ESCAPE, KC_LEFT_SHIFT,   KC_SPC,         LOWER, RAISE, ADJUST
+           KC_LEFT_SHIFT,    KC_SPC,    KC_ENTER        LAYER1,   LOWER,   ADJUST
 ),
 
 
-/* Raise
+/* layer1
  *
  * ,----------------------------------.           ,----------------------------------.
  * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  Tab | Left | Down |  Up  | Right|           |    (  |  )  |   &  |   [  |   ]  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  :  |  SPACE |  X |  C |   V   |           |    {  |  }  |   |  |   "  |   '  |
+ * |  :  |  SPACE |  X |  C |   V     |           |    {  |  }  |   |  |   "  |   '  |
  * `----------------------------------'           `----------------------------------'
  *                  ,--------------------.    ,--------------------.
- *                  |COMMAND| BACKSPACE| ENTER   |      | RAISE|      |
+ *                | ESC |COMMAND| BACKSPACE| |      | RAISE|      |
  *                  `--------------------'    `--------------------.
  */
-[_RAISE] = LAYOUT_split_3x5_3(
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,      KC_LPRN, KC_RPRN, KC_AMPR,  KC_LBRC, KC_RBRC,
-  KC_COLON, KC_SPC,  KC_X, KC_C, KC_V,      KC_LCBR, KC_RCBR, KC_PIPE, KC_DQT,  KC_QUOT,
-                    KC_LCMD, KC_BACKSPACE, KC_ENTER ,      _______, RAISE, _______
+[_LAYER1] = LAYOUT_split_3x5_3(
+  KC_1,     KC_2,    KC_3,      KC_4,           KC_5,               KC_6,       KC_7,       KC_8,       KC_9,       KC_0,
+  KC_TAB,   KC_LEFT, KC_DOWN,   KC_UP,          KC_RGHT,            KC_LPRN,    KC_RPRN,    KC_AMPR,    KC_LBRC,    KC_RBRC,
+  KC_COLON, KC_SPC,  KC_X,      KC_C,           KC_V,               KC_LCBR,    KC_RCBR,    KC_PIPE,    KC_DQT,     KC_QUOT,
+                     KC_ESC,    KC_LCMD,        KC_BACKSPACE,       _______,    RAISE,      _______
 ),
 
 /* Lower
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Reset |      |      |      |      |           |      |   _  |   +  |   {  |   }  |
+ * |  ESC   |      |      |      |    |           |      |   _  |   +  |   {  |   }  |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  Caps|   ~  |      |  <   |   >  |           |      |      |      |   |  |   "  |
  * `----------------------------------'           `----------------------------------'

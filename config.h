affb8d21c7b3bfdac8501e0f713e78fa26ccb7ac
Thilo Barth <thilo.b@rth.pm>
2024-09-12 20:15:37 +0200

    new keyboard

--- config.h
+++ config.h
@@ -15,8 +15,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#pragma once
-
-#define OLED_DISPLAY_128X64
-
 #define EE_HANDS
+
+#define SPLIT_HAND_PIN D3
